f22d888470994fa5d3bf0be6ddbab8018aa04446
Colin Moore <colin@moore.one>
2018-12-09 20:09:40 -0500

    Make canvas fullscreen

--- rasterize.js
+++ rasterize.js
@@ -726,7 +726,8 @@ function setupWebGL() {
 
     // Get the image canvas, render an image in it
     var imageCanvas = document.getElementById("myImageCanvas"); // create a 2d canvas
-    var cw = imageCanvas.width, ch = imageCanvas.height;
+    var cw = imageCanvas.width = window.innerWidth,
+        ch = imageCanvas.height = window.innerHeight;
     imageContext = imageCanvas.getContext("2d");
     var bkgdImage = new Image();
     bkgdImage.crossOrigin = "Anonymous";
@@ -739,6 +740,8 @@ function setupWebGL() {
 
     // Get the canvas and context
     canvas = document.getElementById("myWebGLCanvas"); // create a js canvas
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     gl = canvas.getContext("webgl"); // get a webgl object from it
 
     try {
@@ -929,7 +932,7 @@ function setupShaders() {
 function renderTriangles() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // clear frame/depth buffers
     var transform = mat4.create();
-    mat4.perspective(transform, Math.PI*0.75, canvas.width/canvas.height, 0.01, 100);
+    mat4.perspective(transform, Math.PI*0.5, canvas.width/canvas.height, 0.01, 100);
 
     mat4.multiply(transform, transform, gameState.camera.getTransform());
 
