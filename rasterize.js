26a4b291ef212df52fb36c520bf0e926e7e95fb0
jasminewang0 <jfwang2@ncsu.edu>
2018-12-10 12:41:50 -0500

    added sound effects for death and apple eating

--- rasterize.js
+++ rasterize.js
@@ -29,6 +29,9 @@ var modelInvTransMatrixUniform; // where to put the inverse transpose of the mod
 var gameState = undefined;
 var ControlsEnum = Object.freeze({ "up": 1, "down": 2, "left": 3, "right": 4, "rotateLeft": 5, "rotateRight": 6, "none": 7});
 
+var deathAudio = new Audio('sadtrombone.mp3');
+var appleAudio = new Audio('apple.mp3');
+
 /** GameState class */
 class GameState {
     constructor() {
@@ -56,10 +59,10 @@ class GameState {
         this.snakeUp = vec3.fromValues(0, 0, -1); //straight up
         this.lastControlInput = ControlsEnum.none;
         this.position = vec3.fromValues(0, 0, 0);
-        this.snakePieces = this.createInitialSnake(20);
+        this.snakePieces = this.createInitialSnake(0);
         this.updateScore();
         this.apples = [];
-        this.toGrow = 0;
+        this.toGrow = 20;
 
         this.camera = this.createInitialCamera();
         this.minimapCamera = this.createInitialCamera();
@@ -162,6 +165,7 @@ class GameState {
             vec3.add(sum, this.snakePieces[i], sum);
             if(vec3.length(sum) < 0.1) {
                 document.getElementById("deathScore").innerHTML = this.snakePieces.length;
+                deathAudio.play();
                 showMenu("deathScreen");
                 this.dead = true;
                 vec3.copy(this.position, this.lastTickValues.position); //so that camera doesn't go inside
@@ -178,6 +182,7 @@ class GameState {
             let difference = vec3.create();
             vec3.subtract(difference, this.position, this.apples[i]);
             if(vec3.length(difference) < 0.1) {
+                appleAudio.play();
                 this.toGrow += 20;
                 console.log("Apple eaten!");
                 this.apples.splice(i, 1);
@@ -192,21 +197,8 @@ class GameState {
 
     /** Grows a new apple somewhere on the play field that doesn't collide with the snake */
     growApple() {
-        let applePos = this.getRandomPosition();
-
-        let sum = vec3.clone(this.position);
-        let difference = vec3.create();
-        for (let i = 0; i < this.snakePieces.length; i++) {
-            vec3.add(sum, this.snakePieces[i], sum);
-            vec3.subtract(difference, sum, applePos);
-            if(vec3.length(difference) < 0.1) {
-                this.growApple();
-                return;
-            }
-        }
-        
-        console.log("New apple in position: " + applePos);
-        this.apples.push(applePos);
+        this.apples.push(this.getRandomPosition());
+        // TODO: check to see if this location is within the snake already
     }
     
     /** Returns a random valid position within the playing field */
@@ -216,7 +208,7 @@ class GameState {
         for (let i = 0; i < 3; i++) {
             position.push(Math.floor((Math.random() * max * 2) + 1) - max);
         }
-
+        console.log("New apple in position: " + position);
         return position;
     }
 
